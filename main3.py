b9504f9bd8d1636b084b62769a9b7bbf5ebc36f1
Brian Sharp <briansharp1994@gmail.com>
2019-08-31 17:50:40 -0400

    break up the pytorch implementation into 3 sections

--- main3.py
+++ main3.py
@@ -15,8 +15,9 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
-class AlexModel(nn.Module):
-
+# Section 1
+class Section1(nn.Module):
+    
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 36, kernel_size = 2, padding=1)
@@ -24,39 +25,57 @@ class AlexModel(nn.Module):
         self.pooling1 = nn.AvgPool2d(kernel_size=2)
         self.dropout1 = nn.Dropout(p = 0.25)
 
-        self.conv3 = nn.Conv2d(36, 64, kernel_size = 2, padding = 1)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size = 2)
-        self.pooling2 = nn.AvgPool2d(kernel_size=2)
-        self.dropout2 = nn.Dropout(p = 0.25)
-
-        # Add Flatten in forward and backwards
-        self.flatten = Flatten()
-        self.linear1 = nn.Linear(7 * 7 * 64, 1028)
-        self.dropout3 = nn.Dropout(p = 0.25)
-        self.linear2 = nn.Linear(1028, NUM_CLASSES)
-
     def forward(self, x):
         out = F.relu(self.conv1(x))
         out = F.relu(self.conv2(out))
         out = self.pooling1(out)
         out = self.dropout1(out)
 
-        out = F.relu(self.conv3(out))
+        return out
+
+class Section2(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv3 = nn.Conv2d(36, 64, kernel_size = 2, padding = 1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size = 2)
+        self.pooling2 = nn.AvgPool2d(kernel_size=2)
+        self.dropout2 = nn.Dropout(p = 0.25)
+        self.flatten = Flatten()
+
+    def forward(self, x):
+        out = F.relu(self.conv3(x))
         out = F.relu(self.conv4(out))
         out = self.pooling2(out)
         out = self.dropout2(out)
-
         out = self.flatten(out)
-        out = F.relu(self.linear1(out))
+
+        return out
+
+class Section3(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(7 * 7 * 64, 1028)
+        self.dropout3 = nn.Dropout(p = 0.25)
+        self.linear2 = nn.Linear(1028, NUM_CLASSES)
+
+    def forward(self, x):
+        out = F.relu(self.linear1(x))
         out = self.dropout3(out)
         out = F.softmax(self.linear2(out))
 
         return out
 
-model = AlexModel()
+model1 = Section1()
+model2 = Section2()
+model3 = Section3()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = 5e-4)
+
+optimizer1 = torch.optim.Adam(model1.parameters(), lr = 5e-4)
+optimizer2 = torch.optim.Adam(model2.parameters(), lr = 5e-4)
+optimizer3 = torch.optim.Adam(model3.parameters(), lr = 5e-4)
 
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
@@ -78,14 +97,52 @@ for epoch_iter in range(NUM_EPOCHS):
 
     # train?
     for batch_idx, (data, target) in enumerate(train_loader):
-        optimizer.zero_grad()
-        out = model.forward(data)
-        loss = criterion(out, target)
+        
+        data.requires_grad_(True)
+
+        model1.train()
+        model1.zero_grad()
+        # backwards
+        optimizer1.step()
+        output1 = model1(data)
+        model1.zero_grad()
+
+
+        model2.train()
+        model2.zero_grad()
+        # backwards
+        optimizer2.step()
+        output2_input = output1.detach().clone()
+        output2_input.requires_grad_(True)
+        output2 = model2(output2_input)
+        model2.zero_grad()
+
+        model3.train()
+        model3.zero_grad()
+        optimizer3.step()
+        output3_input = output2.detach().clone()
+        output3_input.requires_grad_(True)
+        output3 = model3(output3_input)
+
+        loss = F.cross_entropy(output3, target)
+        _, predicted = torch.max(output3.data, 1)
+        correct = (predicted == target).sum().item()
+        acc = correct / target.size(0)
+
+        model3.zero_grad()
+
         loss.backward()
-        optimizer.step()
+        output2.backward(output3_input.grad)
+        output1.backward(output2_input.grad)
+
+        optimizer3.step()
+        optimizer2.step()
+        optimizer1.step()
+        
+
         
         # calc accuracy
-        _, indices = out.max(1)
+        _, indices = output3.max(1)
         acc = target.eq(indices).sum().item() / BATCH_SIZE
         print("Acc: ", acc)
 
